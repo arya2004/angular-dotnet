3b672225ef433d9c3a016eef4e22718f922f7c90
arya2004 <arya20j4@gmail.com>
2023-07-18 02:12:57 +0530

    added auth guard

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AccountService } from '../account.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -13,13 +13,15 @@ export class LoginComponent {
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', Validators.required)
   })
-
-  constructor(private accountService: AccountService, private router: Router){}
+  returnUrl: string;
+  constructor(private accountService: AccountService, private router: Router, private activatedRoot: ActivatedRoute){
+    this.returnUrl = this.activatedRoot.snapshot.queryParams['returnUrl'] || '/shop'
+  }
 
   onSubmit()
   {
     this.accountService.login(this.loginForm.value).subscribe({
-      next: () => this.router.navigateByUrl('/shop')
+      next: () => this.router.navigateByUrl(this.returnUrl)
     })
     
   }
